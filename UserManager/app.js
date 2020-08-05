ce4157d87fd2a95e493d7c8927a7ce6a6f48db12
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-08-05 16:05:13 -0600

    this is before the react is added

--- UserManager/app.js
+++ UserManager/app.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const app = express();
+const faker = require('faker')
+faker.seed(123)
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 3000;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const db = 'mongodb://localhost/UserManager'
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-const datab = mongoose.connection;
-const collectionName = 'user'
-const UserSchema = new mongoose.Schema({
-    name: String,
-    role: String,
+const mdb = mongoose.connection;
+const collectionName = 'users'
+const userSchema = new mongoose.Schema({
+    user_id: mongoose.Mixed,
+    first_name: String,
+    last_name: String,
+    email_address: String,
     age: {type: Number, min: 18, max: 100},
-    createdDate: {type: Date, default: Date.now }
-})
-const user = mongoose.model('User', UserSchema, collectionName)
+    password: String,
+    role: {type: String, default: 'user'},
+    createDate: { type: Date, default: Date.now } 
+  })
+const User = mongoose.model('user', userSchema, collectionName)
 
 
-datab.on('error', console.error.bind(console,'connection error:'));
+mdb.on('error', console.error.bind(console,'connection error:'));
 
-datab.once('open', () => {
+mdb.once('open', () => {
     console.log('db connected')
+    //generate()
 })
 
 app.set('views', './views')
@@ -33,11 +41,15 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/addUser', (req, res) => {
-    console.log(`POST /newUser: ${JSON.stringify(req.body)}`);
-    const newUser = new user();
-    newUser.name = req.body.name;
-    newUser.role = req.body.role;
+app.post('/createUser', (req, res) => {
+    console.log(`POST /createUser: ${JSON.stringify(req.body)}`);
+    const newUser = new User();
+    newUser.user_id = uuidv4();
+    newUser.markModified('user_id')
+    newUser.first_name = req.body.firstname
+    newUser.last_name = req.body.lastname
+    newUser.email_address = req.body.email
+    newUser.password = req.body.password
     newUser.age = req.body.age;
     newUser.save((err, data) => {
         if (err) {
@@ -49,11 +61,37 @@ app.post('/addUser', (req, res) => {
 })
 
 
+app.get('/users-list', async (req, res) => {
+    let foundUsers = [];
+    for await (const doc of User.find()) {
+        foundUsers.push(doc); // Prints documents one at a time
+      }
+    res.render('userList', {users: foundUsers})
+})
 
-
+function generate() {
+    for(let i = 0; i <= 12000; i++) {
+        const newUser = new User();
+        newUser.user_id = uuidv4();
+        newUser.markModified('user_id');
+        newUser.first_name = faker.name.firstName()
+        newUser.last_name = faker.name.lastName()
+        newUser.email_address = faker.internet.email()
+        newUser.password = faker.internet.password()
+        newUser.age = faker.random.number({'min': 18,'max': 100})
+        newUser.save((err, data) => {
+            if (err) {
+                return console.log(err)
+            } 
+        })
+    }
+    console.log('generation complete')
+}
 
 
 app.listen(port, (err) => {
      (err) ? console.log(err) : console.log('server is up')
 
 })
+
+
